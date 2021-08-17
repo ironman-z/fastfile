b09adf1cf191ccd99cc5f2d9cbb888775d22d7b7
qhz <qhz@qq.com>
2021-08-17 04:44:10 -0400

    Add lazy static

--- src/services/requests.rs
+++ src/services/requests.rs
@@ -1,6 +1,17 @@
 
 use lazy_static::lazy_static;
+use parking_lot::RwLock;
+use yew::services::storage::{Area, StorageService};
 
-lazy_static {
-    pub static ref TOKEN: 
+const TOKEN_KEY: &str = "yew::token";
+
+lazy_static! {
+    pub static ref TOKEN: RwLock<Option<String>> = {
+        let storage = StorageService::new(Area::Local).expect("storage was disabled")
+        if let Ok(token) = storage.restore("TOKEN_KEY") {
+            RwLocal::new(Some(token))
+        } else {
+            RwLocal::new(None)
+        }
+    };
 }
